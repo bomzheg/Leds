579a8a1b0eba8763a0dab4c1b5562c4a3fce5e54
Yuriy Chebyshev <bomzheg@gmail.com>
2023-06-13 22:52:33 +0300

    added example

--- leds/button_exmple.py
+++ leds/button_exmple.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
+import time
+
+import RPi.GPIO as gpio  # type: ignore[import]
+
+
+LED_PIN = 7
+BUTTON_PIN = 1
 
 
 def main() -> None:
-    return
+    gpio.cleanup()
+    try:
+        gpio.setmode(gpio.BCM)
+        gpio.setup(LED_PIN, gpio.OUT)
+        gpio.output(LED_PIN, gpio.LOW)
+        gpio.setup(BUTTON_PIN, gpio.IN)
+        print("configured")
+        main_loop()
+    finally:
+        gpio.cleanup()
 
 
-if __name__ == '__main__':
+def main_loop() -> None:
+    while True:
+        if gpio.input(BUTTON_PIN):
+            timeout = 0.5
+        else:
+            timeout = 0.1
+            print("key pressed")
+        gpio.output(LED_PIN, gpio.HIGH)
+        time.sleep(timeout)
+        gpio.output(LED_PIN, gpio.LOW)
+        time.sleep(timeout)
+
+
+if __name__ == "__main__":
     main()
