1cb867699c6755132379975024a05d45e5a895d8
Yuriy Chebyshev <bomzheg@gmail.com>
2023-06-15 00:05:46 +0300

    worked all except button

--- leds/button_exmple.py
+++ leds/button_exmple.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import time
 
-import RPi.GPIO as gpio  # type: ignore[import]
+import OPi.GPIO as gpio  # type: ignore[import]
 
 
 LED_PIN = 7
-BUTTON_PIN = 1
+BUTTON_PIN = 12
 
 
 def main() -> None:
     gpio.cleanup()
+    gpio.setwarnings(True)
     try:
-        gpio.setmode(gpio.BCM)
+        gpio.setmode(gpio.BOARD)
         gpio.setup(LED_PIN, gpio.OUT)
         gpio.output(LED_PIN, gpio.LOW)
         gpio.setup(BUTTON_PIN, gpio.IN)
@@ -23,11 +24,11 @@ def main() -> None:
 
 def main_loop() -> None:
     while True:
-        if gpio.input(BUTTON_PIN):
-            timeout = 0.5
-        else:
+        if gpio.input(BUTTON_PIN) == gpio.HIGH:
             timeout = 0.1
             print("key pressed")
+        else:
+            timeout = 0.5
         gpio.output(LED_PIN, gpio.HIGH)
         time.sleep(timeout)
         gpio.output(LED_PIN, gpio.LOW)
