dd106265f33cc05b19ebe0b5bc24e0a2ad5f3d64
Yuriy Chebyshev <bomzheg@gmail.com>
2023-06-15 00:11:22 +0300

    added logging

--- leds/button_exmple.py
+++ leds/button_exmple.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+import logging
 import time
 
 import OPi.GPIO as gpio  # type: ignore[import]
 
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+)
 LED_PIN = 7
 BUTTON_PIN = 12
 
@@ -16,17 +20,18 @@ def main() -> None:
         gpio.setup(LED_PIN, gpio.OUT)
         gpio.output(LED_PIN, gpio.LOW)
         gpio.setup(BUTTON_PIN, gpio.IN)
-        print("configured")
+        logger.warning("configured")
         main_loop()
     finally:
         gpio.cleanup()
+        logger.warning("finished")
 
 
 def main_loop() -> None:
     while True:
         if gpio.input(BUTTON_PIN) == gpio.HIGH:
             timeout = 0.1
-            print("key pressed")
+            logger.debug("key pressed")
         else:
             timeout = 0.5
         gpio.output(LED_PIN, gpio.HIGH)
